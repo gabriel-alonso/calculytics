2b967a054627760e534b082b75e3bcf377811740
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-05-09 16:58:31 -0300

    :hammer: update scripts calculator

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,40 @@
 import Calculator from './modules/Calculator';
 
+/**
+ * Declare interface ElementEvent.
+ * @author Gabriel Alonso
+ */
 declare interface ElementEvent extends Event {
-  currentTarget: HTMLElement;
-  target: HTMLElement;
+  currentTarget: HTMLElement; //alvo atual
+  target: HTMLElement; //alvo posterior
 }
 
-function init() {
-
-  const display: HTMLParagraphElement = document.querySelector('p#display');
-  const calc = new Calculator();
-  const calcBtns = document.querySelectorAll('.calcButton');
+/**
+ * Main Function of the calculator.
+ * @return void
+ * @author Gabriel Alonso
+ */
+function main() {
+  const display: HTMLParagraphElement = document.querySelector('p#display'); //é um interface que fornece propriedades especiais de um <p> elemento.
+  const calc = new Calculator(); //cria um objeto da classe calculadora.
+  const calcBtns = document.querySelectorAll('.calcButton'); //vai retornar as informações desse selector, então nesse caso vai informações sobre o botão.
 
   const handleDisplayUpdate = (val: string) => {
-    display.innerText = val ? val : '0'
+    display.innerText = val ? val : '0';
   };
 
   calc.onDisplayUpdate(handleDisplayUpdate);
 
   const handleBtnClick = (e: ElementEvent) => {
     const el = e.currentTarget;
-    const {
-      value,
-      type
-    } = el.dataset;
+    const {value, type} = el.dataset;
     calc.buttonPressed({
       type,
-      value
-    })
-  }
+      value,
+    });
+  };
 
-  calcBtns.forEach(btn => btn.addEventListener('click', handleBtnClick));
-
+  calcBtns.forEach((btn) => btn.addEventListener('click', handleBtnClick));
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', main);
